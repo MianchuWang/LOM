b7e43c7df4a93ad1ad48b567870eb2cb1ec0dfd5
Mianchu <31563810+MianchuWang@users.noreply.github.com>
2024-10-14 22:51:23 +0100

    Update train.py

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ import logger
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--env_name', type=str, default='halfcheetah-medium-v2')
+parser.add_argument('--env_name', type=str, default='halfcheetah-full-replay-v2')
 parser.add_argument('--agent', type=str, default='LOM')
 parser.add_argument('--buffer_capacity', type=int, default=2000000)
 parser.add_argument('--discount', type=float, default=0.99)
